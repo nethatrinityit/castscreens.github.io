ba9f6032e76e774f0fcf9c4a4e47989e6c254522
nethatrinityit <124787641+nethatrinityit@users.noreply.github.com>
2023-02-14 10:04:47 +0530

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,5 +1,51 @@
 // TODO: Add receiver code here
-const context = cast.framework.CastReceiverContext.getInstance();
-const playerManager = context.getPlayerManager();
+function makeRequest (method, url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
+}
 
+playerManager.setMessageInterceptor(
+    cast.framework.messages.MessageType.LOAD,
+    request => {
+      return new Promise((resolve, reject) => {
+        // Fetch content repository by requested contentId
+        makeRequest('GET', 'https://storage.googleapis.com/cpe-sample-media/content.json').then(function (data) {
+          let item = data[request.media.contentId];
+          if(!item) {
+            // Content could not be found in repository
+            reject();
+          } else {
+            // Add metadata
+            let metadata = new
+               cast.framework.messages.GenericMediaMetadata();
+            metadata.title = item.title;
+            metadata.subtitle = item.author;
+
+            request.media.metadata = metadata;
+
+            // Resolve request
+            resolve(request);
+          }
+        });
+      });
+    });
 context.start();
